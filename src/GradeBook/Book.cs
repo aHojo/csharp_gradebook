4f481b7758c814d724827e90356f489f091621a3
ahojo <ahojodev@gmail.com>
2021-04-01 22:10:56 -0400

    Control Flow

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -6,7 +6,34 @@ namespace GradeBook
     public class Book
     {
         private List<double> grades;
-        public string Name;
+
+        /* Properties Below */
+        /* OLD WAY
+        private string name;
+        public String Name
+        {
+            get
+            {
+                return name; // this is the private field
+            }
+            set
+            {
+                if (!String.IsNullOrEmpty(value))
+                {
+                    name = value; // this variable is implicit, and given to you. 
+                }
+            }
+        }
+        */
+        /* New Way - Auto Property. */
+        public String Name
+        {
+            get;
+            private set;
+        }
+
+        // readonly String category; // can only change in the field or constructor. 
+        public const String CATEGORY = "Science"; // with public here, others can now read it. 
         public void AddGrade(double grade)
         {
             if (grade >= 0 && grade <= 100)
@@ -15,7 +42,30 @@ namespace GradeBook
             }
             else
             {
-                Console.WriteLine("Invalid grade");
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
+        }
+
+        // Overloaded Methods. 
+        public void AddGrade(char letter)
+        {
+            switch (letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
             }
         }
         public Statistics GetStatistics()
@@ -33,6 +83,25 @@ namespace GradeBook
             }
 
             result.Average /= this.grades.Count;
+
+            switch (result.Average)
+            {
+                case var d when d >= 90.0: // d gets assigned the value of result.Average. 
+                    result.Letter = 'A';
+                    break;
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
 
             return result;
         }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -9,13 +9,48 @@ namespace GradeBook
         {
             var book = new Book("Kairi's Gradebook");
 
-            book.AddGrade(89.1);
-            book.AddGrade(90.1);
-            book.AddGrade(99.5);
+            while (true)
+            {
+                Console.WriteLine("Input a grade. (q to quit)");
+                // Get input from the user.
+                var input = Console.ReadLine();
+
+                // If q is entered, then break the loop.
+                if (input.Equals("q"))
+                {
+                    break;
+                }
+
+                try
+                {
+                    // Parse the string into a double
+                    double num = double.Parse(input);
+                    book.AddGrade(num); // add the grade to the book.
+                    // book.AddGrade('A'); // Sending the overloaded method. 
+                }
+                catch (ArgumentException error)
+                {
+                    Console.WriteLine(error.Message); // catching the exception will not crash our program. It will continue.
+                                                      // throw; // Re throws the caught exception. 
+                }
+                finally
+                {
+                    Console.WriteLine("*****************");
+                }
+
+            }
+
+            // book.AddGrade(89.1);
+            // book.AddGrade(90.1);
+            // book.AddGrade(99.5);
 
             Statistics stats = book.GetStatistics();
+
+            Console.WriteLine(Book.CATEGORY);
+            Console.WriteLine($"For the book named {book.Name}");
             Console.WriteLine($"The average grade is {stats.Average:N1}"); // 1 decimal places printed 
             Console.WriteLine($"The lowest grade is => {stats.Low}\nThe highest grade is {stats.High}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
 
         }
     }
